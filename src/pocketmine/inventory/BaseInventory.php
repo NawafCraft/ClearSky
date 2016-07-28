61b22bdb1a73f589ac557e6502e9504a998a194b
dktapps <odigiman@gmail.com>
2016-07-28 12:09:06 +0100

    fd349373a97e2c887ccedd9498e9af916cec7405

--- src/pocketmine/inventory/BaseInventory.php
+++ src/pocketmine/inventory/BaseInventory.php
@@ -116,7 +116,7 @@ abstract class BaseInventory implements Inventory{
 		if($index < 0 or $index >= $this->size){
 			return false;
 		}elseif($item->getId() === 0 or $item->getCount() <= 0){
-			return $this->clear($index);
+			return $this->clear($index, $send);
 		}
 
 		$holder = $this->getHolder();
@@ -131,9 +131,7 @@ abstract class BaseInventory implements Inventory{
 
 		$old = $this->getItem($index);
 		$this->slots[$index] = clone $item;
-		if($send){
-			$this->onSlotChange($index, $old);
-		}
+		$this->onSlotChange($index, $old, $send);
 		
 
 		return true;
@@ -351,9 +349,7 @@ abstract class BaseInventory implements Inventory{
 			}else{
 				unset($this->slots[$index]);
 			}
-			if($send){
-				$this->onSlotChange($index, $old);
-			}
+			$this->onSlotChange($index, $old, $send);
 		}
 
 		return true;
@@ -402,8 +398,10 @@ abstract class BaseInventory implements Inventory{
 		unset($this->viewers[spl_object_hash($who)]);
 	}
 
-	public function onSlotChange($index, $before){
-		$this->sendSlot($index, $this->getViewers());
+	public function onSlotChange($index, $before, $send){
+		if($send){
+			$this->sendSlot($index, $this->getViewers());
+		}
 	}
 
 
